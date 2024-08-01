2409680b54b2118b440f5f255bbdd5ac20089df4
rudra273 <rudramohanty45@gmail.com>
2024-08-02 01:15:13 +0530

    new

--- metrics_collector/script.py
+++ metrics_collector/script.py
@@ -1,53 +1,58 @@
-import time
 from prometheus_client import start_http_server, Gauge
 import psycopg2
-from psycopg2 import sql
+import time
 
-# Database connection parameters
-DB_NAME = "mydb"
-DB_USER = "rudra"
-DB_PASSWORD = "1234"
-DB_HOST = "db"
-DB_PORT = "5432"
+# Define the metrics
+open_gauge = Gauge('stock_open', 'The opening price of the stock', ['symbol'])
+high_gauge = Gauge('stock_high', 'The highest price of the stock', ['symbol'])
+low_gauge = Gauge('stock_low', 'The lowest price of the stock', ['symbol'])
+close_gauge = Gauge('stock_close', 'The closing price of the stock', ['symbol'])
+week_high_gauge = Gauge('stock_week_high', 'The weekly high price of the stock', ['symbol'])
+week_low_gauge = Gauge('stock_week_low', 'The weekly low price of the stock', ['symbol'])
+month_high_gauge = Gauge('stock_month_high', 'The monthly high price of the stock', ['symbol'])
+month_low_gauge = Gauge('stock_month_low', 'The monthly low price of the stock', ['symbol'])
+market_cap_gauge = Gauge('stock_market_cap', 'The market capitalization of the stock', ['symbol'])
+percentage_change_gauge = Gauge('percentage_change', 'The percentage change value', ['symbol'])
 
-# Create a gauge to track stock percentage change
-stock_percentage_change = Gauge('stock_percentage_change', 
-                                'Stock percentage change', 
-                                ['symbol'])
-
-
-def get_stock_data():
-    conn = psycopg2.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT
-    )
+def collect_metrics():
+    # Connect to PostgreSQL
+    conn = psycopg2.connect("dbname=mydb user=rudra password=1234 host=db port=5432")
     cur = conn.cursor()
-    
-    cur.execute(sql.SQL("""
-        SELECT "Symbol", "PercentageChange"
+    # Query to get the stock data
+    cur.execute('''
+        SELECT "Symbol", "Open", "High", "Low", "Close", "WeeklyHigh", "WeeklyLow", "MonthlyHigh", "MonthlyLow", "MarketCap", "PercentageChange"
         FROM stock_data
-    """))
-    
-    results = cur.fetchall()
-    
+    ''')
+    for row in cur.fetchall():
+        symbol = row[0]
+        open_price = row[1]
+        high_price = row[2]
+        low_price = row[3]
+        close_price = row[4]
+        week_high = row[5]
+        week_low = row[6]
+        month_high = row[7]
+        month_low = row[8]
+        market_cap = row[9]
+        percentage_change = row[10]
+
+        # Set the metrics with the column data
+        open_gauge.labels(symbol=symbol).set(open_price)
+        high_gauge.labels(symbol=symbol).set(high_price)
+        low_gauge.labels(symbol=symbol).set(low_price)
+        close_gauge.labels(symbol=symbol).set(close_price)
+        week_high_gauge.labels(symbol=symbol).set(week_high)
+        week_low_gauge.labels(symbol=symbol).set(week_low)
+        month_high_gauge.labels(symbol=symbol).set(month_high)
+        month_low_gauge.labels(symbol=symbol).set(month_low)
+        market_cap_gauge.labels(symbol=symbol).set(market_cap)
+        percentage_change_gauge.labels(symbol=symbol).set(percentage_change)
+
     cur.close()
     conn.close()
-    
-    return results
-
-def update_metrics():
-    for symbol, percentage_change in get_stock_data():
-        stock_percentage_change.labels(symbol=symbol).set(percentage_change)
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
     start_http_server(7878)
-    
-    # Main loop
     while True:
-        update_metrics()
-        print('working')
-        time.sleep(60) 
+        collect_metrics()
+        time.sleep(60)  # Update metrics every 60 seconds
--- stock_backend/stock_backend/settings.py
+++ stock_backend/stock_backend/settings.py
@@ -56,8 +56,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
     "http://localhost:3001", 
-    "http://localhost:3002", 
     "http://localhost:3002", 
 ]
 
