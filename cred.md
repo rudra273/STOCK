493ca5f5b6b3d861f0008d69c698c0d2cdb80ebb
rudra273 <rudramohanty45@gmail.com>
2024-08-02 18:55:26 +0530

    final

--- cred.md
+++
@@ -1,5 +0,0 @@
-host_name = '127.0.0.1'
-database_name = "mydb"
-user_name = 'rudra'
-password_ = '1234'
-port_ = 5432 
---
+++ port.sh
@@ -0,0 +1,6 @@
+kubectl port-forward svc/backend -n stock-app 8002:8000 &
+kubectl port-forward svc/frontend -n stock-app 3002:3000 &
+kubectl port-forward svc/prometheus -n stock-app 9002:9090 &
+kubectl port-forward svc/grafana -n stock-app 3005:3000 &
+
+echo "All services are deployed and port forwarding is set up."
